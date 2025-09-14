b38412eac286b1fa8dbfcbbf2ec315d1d781cafe
gitdevjin <dev.hjshin@gmail.com>
2025-09-14 18:56:13 -0400

    updated

--- src/app/(wrapper)/client/dm/page.tsx
+++ src/app/(wrapper)/client/dm/page.tsx
@@ -2,10 +2,6 @@ import { DirectMessage, GroupChat } from "@/types/entity-type.ts/user";
 import { cookies } from "next/headers";
 
 export default async function DmPage() {
-  //left panel require a list of direct messages
-  //to get a list of dm, it needs accesstoken,
-  //using cookie get a list of dms and also implement friend button.
-
   const cookieList = await cookies();
   const accesstoken = cookieList.get("accessToken")?.value;
 
@@ -16,34 +12,28 @@ export default async function DmPage() {
     },
   });
 
-  if (!userRes) {
+  if (!userRes.ok) {
     throw new Error("User not authenticated");
   }
 
   const user = await userRes.json();
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_TNT_SERVER_URL}/chats/dms`, {
+  const dmRes = await fetch(`${process.env.NEXT_PUBLIC_TNT_SERVER_URL}/chats/dms`, {
     method: "GET",
     headers: {
       cookie: `accessToken=${accesstoken}`,
     },
   });
 
-  if (!res.ok) {
+  if (!dmRes.ok) {
     throw new Error("Failed to fetch DMs");
   }
 
-  const listOfDms: DirectMessage[] = await res.json();
+  const dms: DirectMessage[] = await dmRes.json();
 
-  console.log(listOfDms);
-  console.log(user.id);
-
-  const friendNames = listOfDms.map((dm) => {
+  const friendNames = dms.map((dm) => {
     return dm.members.find((m) => m.userId !== user.id)?.user.username;
   });
-
-  console.log("---------------");
-  console.log(friendNames);
 
   return (
     <div className="bg-gray-300 w-full h-full border-1">
@@ -51,8 +41,8 @@ export default async function DmPage() {
         <div>DM Page 'resizable'</div>
         <div className="flex flex-row gap-10 w-full h-full">
           <div className="border-1 w-50 p-5 h-full">
-            {friendNames.map((friend: any) => {
-              return <div key={friend}>{friend}</div>;
+            {friendNames.map((fName: any) => {
+              return <div key={fName}>{fName}</div>;
             })}
           </div>
           <div className="border-1 p-10 w-full h-full">right</div>
